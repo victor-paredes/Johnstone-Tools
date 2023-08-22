2783e891802149d872a8d0701768426db0eb3094
Victor Paredes <victor@paredescreative.com>
2023-08-22 14:19:16 -0700

    updating

--- parts/js_header.php
+++ parts/js_header.php
@@ -5,5 +5,7 @@
 
 <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/styles/johnstone.css">
 
-
 <div class="js_entire_wrap">
+
+    <img src="http://victorpared.es/js/wp-content/uploads/2023/08/johnstone_header_v1.png">
+    <br>
