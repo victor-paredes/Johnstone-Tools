6b6d4de325e6fda90ef832f554b65f6dba0e3b26
Victor Paredes <victor@paredescreative.com>
2023-08-22 14:54:29 -0700

    updating

--- sections/standard_template_product_cards.php
+++ sections/standard_template_product_cards.php
@@ -24,7 +24,12 @@
                 
                 <div class="card">
                 <div align="center" style="min-height:260px">
-                <h4 style="background-color: #1F3962; color: #fff">Featured Product</h4>
+                
+                <?php if (true) { ?>
+                    <?php echo get_field( 'featured_product' ); ?>
+                    <h4 style="background-color: #1F3962; color: #fff">Featured Product</h4>
+                <?php } ?>
+
                 <a href="https://www.johnstonesupply.com/product-view?pID=H28-669"><img data-type="hippogallery:original" data-uuid="5b9744fb-51d0-4b00-b7b8-819c66bd195f" src="binaries/content/gallery/johnstonesupplypages/friedrich-product-offering/ptac-front_2000.jpg/ptac-front_2000.jpg/hippogallery:original" style="width: 80%" /> </a></div>
                 
                 <div class="card-body d-flex flex-column">
