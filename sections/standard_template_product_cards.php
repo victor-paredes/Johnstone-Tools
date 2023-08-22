4b47755541141a2009e1865761213f19e1feeeb8
Victor Paredes <victor@paredescreative.com>
2023-08-22 15:12:00 -0700

    updating

--- sections/standard_template_product_cards.php
+++ sections/standard_template_product_cards.php
@@ -12,13 +12,19 @@
 
         if ( $sections ) :
             foreach ( $sections as $section ) :
-
         ?>
 
 
 
 
+            <?php if( get_row_layout() == 'standard_template_product_cards' ): ?>
+                <?php the_sub_field('product_title'); ?>
+            <?php endif; ?>
 
+
+
+
+                
 
 
                 
@@ -26,7 +32,7 @@
                 <div align="center" style="min-height:260px">
                 
                 <?php if (true) { ?>
-                    answer: <?php echo get_field( 'featured_product' ); ?>
+                    answer: <?php echo get_sub_field( 'featured_product' ); ?>
                     <h4 style="background-color: #1F3962; color: #fff">Featured Product</h4>
                 <?php } ?>
 
@@ -50,7 +56,6 @@
                 <p style="margin:0in 0in 8pt"><span style="font-size:11pt"><span style="line-height:107%"><span style="font-family:Calibri,sans-serif"><span style="font-size:10.0pt"><span style="line-height:107%"></span></span></span></span></span></p>
                 <a class="btn btn-primary mt-auto" href="https://www.johnstonesupply.com/search/go?w=B76-099,%20B76-100,%20B76-101,%20B76-102,%20B76-107,%20B76-108,%20B76-109,%20B76-110,%20B76-103,%20B76-111,%20B76-104,%20B76-112,%20B76-105,%20B76-113,%20B76-106,%20B76-114" role="button" target="_blank">Learn More </a></div>
                 </div>
-
 
 
 
