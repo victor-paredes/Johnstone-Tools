e37c79c63beb9e4f552a41e5f9d8186c4f03d4da
Victor Paredes <victor@paredescreative.com>
2023-08-22 14:15:24 -0700

    updating

--- parts/js_header.php
+++ parts/js_header.php
@@ -1,6 +1,9 @@
 <?php
-    wp_head();
-    wp_enqueue_style('style', get_template_directory_uri() . '/styles/johnstone.css');
+    // wp_enqueue_style('style', get_template_directory_uri() . '/styles/johnstone.css');
+    
 ?>
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>styles/johnstone.css">
+
 
 <div class="js_entire_wrap">
