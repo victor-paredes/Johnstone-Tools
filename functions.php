3da9b46b17e55ac5a24002c4ab3a8b0dde153001
Victor Paredes <victor@paredescreative.com>
2023-08-22 09:49:16 -0700

    adding styles and functions.php

---
+++ functions.php
@@ -0,0 +1,2 @@
+wp_enqueue_style ('theme-style', get_template_directory_uri().'/css/johnstone.css');
+wp_enqueue_style ('my-style', get_template_directory_uri().'/css/johnstone.css', array('theme-style'));
