9908a664fb42a595b82ae5b3db6e0e3699228c59
Victor Paredes <victor@paredescreative.com>
2023-08-22 13:25:43 -0700

    updating to non-blocks theme

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -4,9 +4,28 @@ Template Name: Johnstone Template
 Template Post Type: landing-page
 */
 
-
-echo 'test';
 ?>
 
 <br />
-<?php echo the_content(); ?>
+<?php // echo the_content(); ?>
+
+<?php 
+
+/* Start the Loop */
+while ( have_posts() ) :
+    the_post();
+
+    // ACF - Flexible Content fields.
+    $sections = get_field( 'standard_template_blocks' );
+
+    if ( $sections ) :
+        foreach ( $sections as $section ) :
+            test
+            //get_template_part( 'flexible-content/sections/' . $template, '', $section );
+        endforeach;
+    endif;
+
+endwhile; // End of the loop.
+
+
+
