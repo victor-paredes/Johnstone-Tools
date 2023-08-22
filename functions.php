24c2645866532e717700bde5c0a91c5e07584a6c
Victor Paredes <victor@paredescreative.com>
2023-08-22 09:52:49 -0700

    update

--- functions.php
+++ functions.php
@@ -2,6 +2,4 @@
 
 // add stylesheets
 wp_enqueue_style ('theme-style', get_template_directory_uri().'/css/johnstone.css');
-// wp_enqueue_style ('my-style', get_template_directory_uri().'/css/johnstone.css', array('theme-style'));
-
 ?>
