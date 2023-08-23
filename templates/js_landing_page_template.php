ab8aac5fbaca7eedf837579498d88144860f1c86
Victor Paredes <victor@paredescreative.com>
2023-08-22 19:20:20 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -19,7 +19,7 @@ if ( have_rows('standard_template_blocks') == true ) {
 
         if ( $current_layout == 'header' ) {
             get_template_part( 'sections/standard_template_header' );
-        } elseif ( $current_layout == 'about_section' ) {
+        } elseif ( $current_layout == 'standard_template_about_section' ) {
             get_template_part( 'sections/standard_template_about_section' );
         } elseif ( $current_layout == 'standard_template_product_cards' ) {
             get_template_part( 'sections/standard_template_product_cards' );
