165cc7638f216189dc284aafa3f47c99a75389da
Victor Paredes <victor@paredescreative.com>
2023-08-22 18:02:15 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -16,19 +16,21 @@ if ( have_rows('standard_template_blocks') == true ) {
     while( have_rows('standard_template_blocks') ) {
         the_row();
 
+        $current_layout = get_row_layout();
+
         //echo get_row_layout();
 
-        if ( get_row_layout() == 'header_image' ) {
+        if ( $current_layout == 'header_image' ) {
 
             echo 'header';
             //get_template_part( 'sections/standard_template_product_cards' );
 
-        } elseif ( get_row_layout() == 'about_section' ) {
+        } elseif ( $current_layout == 'about_section' ) {
 
             echo 'about_section';
             //get_template_part( 'sections/standard_template_product_cards' );
 
-        } elseif ( get_row_layout() == 'standard_template_product_cards' ) {
+        } elseif ( $current_layout == 'standard_template_product_cards' ) {
 
             echo 'standard_template_product_cards .';
             //get_template_part( 'sections/standard_template_product_cards' );
