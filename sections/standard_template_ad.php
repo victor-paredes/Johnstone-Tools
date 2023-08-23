7a979d59d5ce2533d2eafe31926bf68a4787b1e6
Victor Paredes <victor@paredescreative.com>
2023-08-22 21:20:04 -0700

    updating

--- sections/standard_template_ad.php
+++ sections/standard_template_ad.php
@@ -3,7 +3,7 @@
 
 <div class="header_image_wrap container">
     <?php if ( $ad_link ) { ?>
-        <a href="<?php if ( $ad_link ) { ?>">
+        <a href="<?php echo $ad_link; ?>">
     <?php } ?>
 
         <img src="<?php the_sub_field('ad_image_url'); ?>">
