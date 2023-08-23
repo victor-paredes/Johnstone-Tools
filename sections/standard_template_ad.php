d4c364c61822626a172f73963de0a4afefe262c5
Victor Paredes <victor@paredescreative.com>
2023-08-22 21:13:51 -0700

    updating

--- sections/standard_template_ad.php
+++ sections/standard_template_ad.php
@@ -1,4 +1,11 @@
 
 <div class="header_image_wrap container">
-    <img src="<?php the_sub_field('ad_image_url'); ?>">
+    <?php if ( get_sub_field(ad_link) ) { ?>
+        <a href="<?php if ( get_sub_field(ad_link) ) { ?>">
+    <?php } ?>
+
+        <img src="<?php the_sub_field('ad_image_url'); ?>">
+    <?php if ( get_sub_field(ad_link) ) { ?>
+        </a>
+    <?php } ?>
 </div>
--- sections/standard_template_header.php
+++
@@ -1,4 +0,0 @@
-
-<div class="header_image_wrap container">
-    <img src="<?php the_sub_field('image'); ?>">
-</div>
