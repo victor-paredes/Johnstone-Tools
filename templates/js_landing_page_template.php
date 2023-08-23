c49d056d9b744dbf4286d1b3a3bcdd41c9828a98
Victor Paredes <victor@paredescreative.com>
2023-08-22 17:46:07 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -16,9 +16,13 @@ if ( have_rows('standard_template_blocks') == true ) {
     while( have_rows('standard_template_blocks') ) {
         the_row();
 
+        if ( get_row_layout() == 'standard_template_product_cards' ) {
+            echo 'product cards layout';
+        } else {
+            echo 'not';
+        }
         //get_template_part( 'sections/standard_template_product_cards' );
     
-        echo 'true';
 
     }
 
