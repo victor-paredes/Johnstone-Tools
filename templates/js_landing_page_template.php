615b16de21d7a7ef82cd17e95e7a38a6d4f72a53
Victor Paredes <victor@paredescreative.com>
2023-08-22 17:20:18 -0700

    updating functions.php to remove superfluous post type

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -11,19 +11,15 @@ get_template_part( 'parts/js_header' );
 
 
 
+if ( get_field(standard_template_blocks) == true ) {
+    echo 'true';
+} else {
+    echo 'false';
+}
 
-
-
-    get_template_part( 'sections/standard_template_product_cards' );
+get_template_part( 'sections/standard_template_product_cards' );
 
     
-
-
-
-
-
-
-
 
 
 
