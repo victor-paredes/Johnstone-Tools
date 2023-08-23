181508375b5b5ff9436d227577124e10f0434c88
Victor Paredes <victor@paredescreative.com>
2023-08-22 18:49:14 -0700

    updating

--- sections/standard_template_product_cards.php
+++ sections/standard_template_product_cards.php
@@ -32,7 +32,7 @@
 
                     <div align="center" style="min-height:260px">
                         <?php if ( get_sub_field( 'featured' ) == true) { ?>
-                            <h4 class="featured_product_wrap" style="background-color: #1F3962; color: #fff">
+                            <h4 style="background-color: #1F3962; color: #fff">
                                 Featured Product
                             </h4>
                         <?php } ?>
@@ -48,17 +48,12 @@
                             <?php the_sub_field('product_title'); ?>
                         </h5>
                         
-                        <h6>Quiestest* standard PTAC you can buy</h6>
+                        <h6>
+                            <?php the_sub_field('product_subtitle'); ?>
+                        </h6>
+
+                        <?php the_sub_field('product_description'); ?>
                         
-                        <ul>
-                            <li>Up to 15,000 Btus</li>
-                            <li>R32 Models (CA Compliant)</li>
-                            <li>208/230V &amp; 265V models</li>
-                            <li>Electric Heat and Heat Pump, up to 13.0 EER</li>
-                            <li>Universal power cords enable properties to select the appropriate heater size</li>
-                            <li>Diamonblue Advanced Corrosion Protection®</li>
-                            <li>Onboard Controls</li>
-                        </ul>
                         
                         <p style="margin:0in 0in 8pt"><span style="font-size:11pt"><span style="line-height:107%"><span style="font-family:Calibri,sans-serif"><span style="font-size:10.0pt"><span style="line-height:107%"></span></span></span></span></span></p>
                         <a class="btn btn-primary mt-auto" href="https://www.johnstonesupply.com/search/go?w=B76-099,%20B76-100,%20B76-101,%20B76-102,%20B76-107,%20B76-108,%20B76-109,%20B76-110,%20B76-103,%20B76-111,%20B76-104,%20B76-112,%20B76-105,%20B76-113,%20B76-106,%20B76-114" role="button" target="_blank">
