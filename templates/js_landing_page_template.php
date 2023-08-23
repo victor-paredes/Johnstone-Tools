c3b7386c78ba7e61417626c399f7088c3d778bdb
Victor Paredes <victor@paredescreative.com>
2023-08-22 18:13:50 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -15,35 +15,21 @@ if ( have_rows('standard_template_blocks') == true ) {
 
     while( have_rows('standard_template_blocks') ) {
         the_row();
-
         $current_layout = get_row_layout();
 
-        //echo get_row_layout();
-
         if ( $current_layout == 'header_image' ) {
-
             echo 'header';
-            //get_template_part( 'sections/standard_template_product_cards' );
-
         } elseif ( $current_layout == 'about_section' ) {
-
             echo 'about_section';
-            //get_template_part( 'sections/standard_template_product_cards' );
-
         } elseif ( $current_layout == 'standard_template_product_cards' ) {
-
-            //echo 'standard_template_product_cards .';
             get_template_part( 'sections/standard_template_product_cards' );
-
         }
 
         echo '<br>';
-
     }
-
     
 } else {
-    echo 'false';
+    echo 'no rows';
 }
 
 
