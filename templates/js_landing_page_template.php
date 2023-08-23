03a8bf368f6b51e1231c1c5efa8b04f472926460
Victor Paredes <victor@paredescreative.com>
2023-08-22 20:59:17 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -15,21 +15,23 @@ if ( have_rows('standard_template_blocks') == true ) { ?>
 
     <div class="COPY THIS CONTAINER TO BLOOMREACH">
 
-        while( have_rows('standard_template_blocks') ) {
-            the_row();
-            $current_layout = get_row_layout();
+        <?php
+            while( have_rows('standard_template_blocks') ) {
+                the_row();
+                $current_layout = get_row_layout();
 
-            if ( $current_layout == 'header' ) {
-                get_template_part( 'sections/standard_template_header' );
-            } elseif ( $current_layout == 'about_section' ) {
-                get_template_part( 'sections/standard_template_about_section' );
-            } elseif ( $current_layout == 'standard_template_product_cards' ) {
-                get_template_part( 'sections/standard_template_product_cards' );
-            } elseif ( $current_layout == 'additional_resources' ) {
-                get_template_part( 'sections/standard_template_additional_resources' );
+                if ( $current_layout == 'header' ) {
+                    get_template_part( 'sections/standard_template_header' );
+                } elseif ( $current_layout == 'about_section' ) {
+                    get_template_part( 'sections/standard_template_about_section' );
+                } elseif ( $current_layout == 'standard_template_product_cards' ) {
+                    get_template_part( 'sections/standard_template_product_cards' );
+                } elseif ( $current_layout == 'additional_resources' ) {
+                    get_template_part( 'sections/standard_template_additional_resources' );
+                }
+
             }
-
-        }
+        ?>
 
     </div>
     
