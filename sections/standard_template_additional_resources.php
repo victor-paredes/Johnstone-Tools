f87538230ba71288e157f1d75dffe45b62b5bfc2
Victor Paredes <victor@paredescreative.com>
2023-08-22 19:53:13 -0700

    updating

--- sections/standard_template_additional_resources.php
+++ sections/standard_template_additional_resources.php
@@ -10,7 +10,8 @@
 
 
     <div class="simple-content-container">
-<style type="text/css">.contain_js {
+<style type="text/css">
+.contain_js {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -38,7 +39,17 @@
         text-decoration: none;
     }
 </style>
-<!--Additional Resources-->
+
+
+
+<?php 
+    while( have_rows('resource') ){ 
+        the_row(); 
+        echo 'row <br>';
+    }
+?>
+
+
 <section class="contain_js">
 <div class="addl-resources">
 <p style="font-family: Helvetica, Arial, sans-serif; color: #fff; font-weight: bold; font-size: 14pt;">Additional<br>
@@ -59,11 +70,7 @@ Counter Mat</a></p>
 Mini-Split Selector Tool</a></p>
 </div>
 </section>
-<!--OLD
-<div class="row">
-<div class="col" style="text-align: center;"><img data-type="hippogallery:original" data-uuid="f4492285-2d62-4100-bb41-0304ea4679d7" src="/pagenotfound" /> <a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/2020%20Johnstone%20Products%20Book.pdf" target="_blank"> <img data-type="hippogallery:original" data-uuid="c483f0f5-ce9b-4039-9651-4703c583f416" src="/pagenotfound" /></a> <a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/Johnstone%20Countermat_2020.pdf" target="_blank"><img data-type="hippogallery:original" data-uuid="2aca52de-ff9e-4b05-8fc9-5fae365a3b88" src="/pagenotfound" /></a><a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/Johnstone%20Key%20Ring%20Sell%20Sheets_2020.pdf" target="_blank"><img data-type="hippogallery:original" data-uuid="a69a8f78-2ff5-4099-a549-009c8ab7ab20" src="/pagenotfound" /></a><a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/2020%20Friedrich%20Inside%20Sales%20and%20Rep%20Contacts.pdf" target="_blank"><img data-type="hippogallery:original" data-uuid="e97aaf13-9d18-4db6-ba02-90cf419082c0" src="/pagenotfound" /></a></div>
 </div>
--->    </div>
                 </div>
         </div>
     </div>
