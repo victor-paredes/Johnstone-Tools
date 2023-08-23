1f9d4d4d6de662decab6815df26ed0c064515fbd
Victor Paredes <victor@paredescreative.com>
2023-08-22 20:00:18 -0700

    updating

--- sections/standard_template_additional_resources.php
+++ sections/standard_template_additional_resources.php
@@ -1,7 +1,8 @@
 <div class="container-fluid  my-2 columnsCollectionContainer">
     <div class="container my-3">
         <div class="row">
-                <div class="col-md">
+            <div class="col-md">
+                <div class="simple-content-container">
 
 
 
@@ -9,69 +10,68 @@
 
 
 
-    <div class="simple-content-container">
-<style type="text/css">
-.contain_js {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 5px, 30px;
-    margin: 30px;
-    text-align: center;
-  }
-    .addl-resources {
-    display: inline-block;
-    justify-content: center;
-    align-items: center;
-    border-left: 1px solid #fff;
-    width: 25%;
-    height: 90px;
-    padding-top: 12pt;
-    padding-bottom: 5pt;
-    padding-right: 10px;
-    padding-left: 10px;
-    margin: 0pt;
-    text-align: center;
-    background-color: #004990;
- }
-    a.js {
-        color: #fff;
-        text-decoration: none;
-    }
-</style>
+                    <style type="text/css">
+                        .contain_js {
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            padding: 5px, 30px;
+                            margin: 30px;
+                            text-align: center;
+                        }
+                        .addl-resources {
+                            display: inline-block;
+                            justify-content: center;
+                            align-items: center;
+                            border-left: 1px solid #fff;
+                            width: 25%;
+                            height: 90px;
+                            padding-top: 12pt;
+                            padding-bottom: 5pt;
+                            padding-right: 10px;
+                            padding-left: 10px;
+                            margin: 0pt;
+                            text-align: center;
+                            background-color: #004990;
+                        }
+                        a.js {
+                            color: #fff;
+                            text-decoration: none;
+                        }
+                    </style>
 
 
 
-<?php 
-    while( have_rows('resource') ){ 
-        the_row(); 
-        echo 'row <br>';
-    }
-?>
 
 
-<section class="contain_js">
-<div class="addl-resources">
-<p style="font-family: Helvetica, Arial, sans-serif; color: #fff; font-weight: bold; font-size: 14pt;">Additional<br>
-Resources</p>
-</div>
+                    <section class="contain_js">
 
-<div class="addl-resources">
-<p style="font-family: Helvetica, Arial, sans-serif; color: #fff;"><a class="js" target="_blank" href="/binaries/content/assets/johnstonesupplypages/friedrich/2023-johnstoneproductsbook_rev_6-13-23.pdf">Friedrich’s Johnstone Supply Product Book</a></p>
-</div>
 
-<div class="addl-resources">
-<p style="font-family: Helvetica, Arial, sans-serif; color: #fff;"><a class="js" target="_blank" title="Johnstone Counter Mat Insert" href="/binaries/content/assets/johnstonesupplypages/friedrich/johnstonecountermat-2023-insert.pdf">Ductless Mini-Split<br>
-Counter Mat</a></p>
-</div>
+                        <?php 
+                            while( have_rows('resource') ){ 
+                                the_row(); 
+                        ?>
+                            <div class="addl-resources">
+                                <p style="font-family: Helvetica, Arial, sans-serif; color: #fff; font-weight: bold; font-size: 14pt;">
+                                    <?php the_sub_field('resource_title'); ?>
+                                </p>
+                            </div>
+                                
+                        <?php
+                            }
+                        ?>
 
-<div class="addl-resources">
-<p style="font-family: Helvetica, Arial, sans-serif; color: #fff; padding-left: 0px;"><a class="js" href="https://www.johnstonesupply.com/mini-split-selector" target="_blank">Johnstone Supply<br>
-Mini-Split Selector Tool</a></p>
-</div>
-</section>
-</div>
+
+                        
+                    </section>
+
+
+
+
+
+
                 </div>
+            </div>
         </div>
     </div>
 </div>
