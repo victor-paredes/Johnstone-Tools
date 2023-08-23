5e9cbc1284dbc52c9ded94ab000c9230ae443f00
Victor Paredes <victor@paredescreative.com>
2023-08-22 19:42:07 -0700

    updating

--- parts/js_footer.php
+++ parts/js_footer.php
@@ -1,7 +1,16 @@
 </div> <!-- js_entire_wrap -->
 
 
-<img src="http://victorpared.es/js/wp-content/uploads/2023/08/johnstonesupplycms_xl.png">
+
+<div class="container-fluid  my-2 columnsCollectionContainer">
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-md">    
+                <img class="w-100 img-fluid align-center" src="/binaries/content/gallery/johnstonesupplypages/coleman/poj_bottom-banner-01-01.png" srcset="/binaries/content/gallery/johnstonesupplypages/coleman/poj_bottom-banner-01-01.png/poj_bottom-banner-01-01.png/johnstonesupplycms%3Asm 450w,/binaries/content/gallery/johnstonesupplypages/coleman/poj_bottom-banner-01-01.png/poj_bottom-banner-01-01.png/johnstonesupplycms%3Amd 690w,/binaries/content/gallery/johnstonesupplypages/coleman/poj_bottom-banner-01-01.png/poj_bottom-banner-01-01.png/johnstonesupplycms%3Axl 1110w" alt="Thats the Power of Johnstone™">
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <div class="js_footer_wrap"></div>
