1decccc4561d34812512c7827d8ae2e7ebce8efc
Victor Paredes <victor@paredescreative.com>
2023-08-22 20:16:30 -0700

    updating

--- sections/standard_template_about_section.php
+++ sections/standard_template_about_section.php
@@ -1,7 +1,7 @@
 <div class="container-fluid  my-2 columnsCollectionContainer">
     <div class="container my-3">
         <div class="row">
-            <div class="col-md">
+                <div class="col-md">
 
 
 
@@ -9,36 +9,47 @@
 
 
 
-                <div class="simple-content-container">
-                    <div class="card-deck">
-                        <div class="card" style="">
-                            <div align="center">
-                                <h1 style="background-color: #005e90; color: #fff; font-size: 15.0pt; padding: 8px;">
-                                    <?php the_sub_field('about_title'); ?>
-                                </h1>
+    <div class="simple-content-container">
+<div class="card-deck">
+<div class="card" style="">
+<div align="center">
+<h1 style="background-color: #005e90; color: #fff; font-size: 15.0pt; padding: 8px;"><?php the_sub_field('about_title'); ?></h1>
 
-                                <div class="card-body">
-                                    <h2 style="margin: 0in 0in 10pt; text-align: left; line-height: 112%; font-size: 12pt; font-family: Arial, Helvetica, sans-serif;" type="text/css">
-                                        <strong>
-                                            <?php the_sub_field('tagline'); ?>
-                                        </strong>
-                                    </h2>
-                                    <p style="margin: 0in 0in 10pt; text-align: left; line-height:112%;font-size:14pt;color:#000;font-family:Calibri,sans-serif" type="text/css">
-                                        <?php the_sub_field('about_us_description'); ?>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>    
-                    </div>
+<div class="card-body">
+<h2 style="margin: 0in 0in 10pt; text-align: left; line-height: 112%; font-size: 12pt; font-family: Arial, Helvetica, sans-serif;" type="text/css"><strong><?php the_sub_field('tagline'); ?></strong></h2>
+
+<p style="margin: 0in 0in 10pt; text-align: left; line-height:112%;font-size:14pt;color:#000;font-family:Calibri,sans-serif" type="text/css"><?php the_sub_field('about_us_description'); ?></p>
+</div>
+<span style="" type="text/css"> </span></div>
+</div>
+</div>    </div>
                 </div>
-                
-                
                 <div class="col-md mt-3 mt-sm-0">
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/jt8fsxbdGEk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
-                    </div>
+
+
+
+
+
+
+
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="<?php the_sub_field('video_url'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        </div>
+
                 </div>
-            
         </div>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+                                        
+                                    
