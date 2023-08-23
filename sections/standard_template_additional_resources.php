a3799bd4adebc9f00b3d2b045d2d9f7569b1469b
Victor Paredes <victor@paredescreative.com>
2023-08-22 19:45:16 -0700

    updating

---
+++ sections/standard_template_additional_resources.php
@@ -0,0 +1,70 @@
+<div class="container-fluid  my-2 columnsCollectionContainer">
+    <div class="container my-3">
+        <div class="row">
+                <div class="col-md">
+
+
+
+
+
+
+
+    <div class="simple-content-container">
+<style type="text/css">.contain_js {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px, 30px;
+    margin: 30px;
+    text-align: center;
+  }
+    .addl-resources {
+    display: inline-block;
+    justify-content: center;
+    align-items: center;
+    border-left: 1px solid #fff;
+    width: 25%;
+    height: 90px;
+    padding-top: 12pt;
+    padding-bottom: 5pt;
+    padding-right: 10px;
+    padding-left: 10px;
+    margin: 0pt;
+    text-align: center;
+    background-color: #004990;
+ }
+    a.js {
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
+<!--Additional Resources-->
+<section class="contain_js">
+<div class="addl-resources">
+<p style="font-family: Helvetica, Arial, sans-serif; color: #fff; font-weight: bold; font-size: 14pt;">Additional<br>
+Resources</p>
+</div>
+
+<div class="addl-resources">
+<p style="font-family: Helvetica, Arial, sans-serif; color: #fff;"><a class="js" target="_blank" href="/binaries/content/assets/johnstonesupplypages/friedrich/2023-johnstoneproductsbook_rev_6-13-23.pdf">Friedrich’s Johnstone Supply Product Book</a></p>
+</div>
+
+<div class="addl-resources">
+<p style="font-family: Helvetica, Arial, sans-serif; color: #fff;"><a class="js" target="_blank" title="Johnstone Counter Mat Insert" href="/binaries/content/assets/johnstonesupplypages/friedrich/johnstonecountermat-2023-insert.pdf">Ductless Mini-Split<br>
+Counter Mat</a></p>
+</div>
+
+<div class="addl-resources">
+<p style="font-family: Helvetica, Arial, sans-serif; color: #fff; padding-left: 0px;"><a class="js" href="https://www.johnstonesupply.com/mini-split-selector" target="_blank">Johnstone Supply<br>
+Mini-Split Selector Tool</a></p>
+</div>
+</section>
+<!--OLD
+<div class="row">
+<div class="col" style="text-align: center;"><img data-type="hippogallery:original" data-uuid="f4492285-2d62-4100-bb41-0304ea4679d7" src="/pagenotfound" /> <a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/2020%20Johnstone%20Products%20Book.pdf" target="_blank"> <img data-type="hippogallery:original" data-uuid="c483f0f5-ce9b-4039-9651-4703c583f416" src="/pagenotfound" /></a> <a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/Johnstone%20Countermat_2020.pdf" target="_blank"><img data-type="hippogallery:original" data-uuid="2aca52de-ff9e-4b05-8fc9-5fae365a3b88" src="/pagenotfound" /></a><a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/Johnstone%20Key%20Ring%20Sell%20Sheets_2020.pdf" target="_blank"><img data-type="hippogallery:original" data-uuid="a69a8f78-2ff5-4099-a549-009c8ab7ab20" src="/pagenotfound" /></a><a href="https://cdn2.hubspot.net/hubfs/1669395/2020%20Literature%20PDFs/Misc.%20Professional%20Documents/2020%20Friedrich%20Inside%20Sales%20and%20Rep%20Contacts.pdf" target="_blank"><img data-type="hippogallery:original" data-uuid="e97aaf13-9d18-4db6-ba02-90cf419082c0" src="/pagenotfound" /></a></div>
+</div>
+-->    </div>
+                </div>
+        </div>
+    </div>
+</div>
