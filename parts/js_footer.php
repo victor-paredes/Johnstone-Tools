94e703fbc3b8f60b6e0fa16e4bf0e953f09a6138
Victor Paredes <victor@paredescreative.com>
2023-08-22 19:38:42 -0700

    updating

--- parts/js_footer.php
+++ parts/js_footer.php
@@ -1,6 +1,9 @@
 </div> <!-- js_entire_wrap -->
 
 
+<img src="http://victorpared.es/js/wp-content/uploads/2023/08/johnstonesupplycms_xl.png">
+
+
 <div class="js_footer_wrap"></div>
 <div class="js_bottom_wrap"></div>
 
