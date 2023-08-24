1a90b05e84c74a9487651e797e8137c2511470a3
Victor Paredes <victor@paredescreative.com>
2023-08-24 08:13:55 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -6,45 +6,47 @@ Template Post Type: landing-page
 
 // echo the_content();
 
-get_template_part( 'parts/js_header' );
+get_template_part( 'parts/js_header' ); ?>
 
 
+<div class="container">
 
+<?php
+    if ( have_rows('standard_template_blocks') == true ) { ?>
 
-if ( have_rows('standard_template_blocks') == true ) { ?>
+        <div class="COPY_THIS_CONTAINER_TO_BLOOMREACH">
 
-    <div class="COPY_THIS_CONTAINER_TO_BLOOMREACH">
+            <?php
+                while( have_rows('standard_template_blocks') ) {
+                    the_row();
+                    $current_layout = get_row_layout();
 
-        <?php
-            while( have_rows('standard_template_blocks') ) {
-                the_row();
-                $current_layout = get_row_layout();
+                    if ( $current_layout == 'ad' ) {
+                        get_template_part( 'sections/standard_template_ad' );
+                    } elseif ( $current_layout == 'about_section' ) {
+                        get_template_part( 'sections/standard_template_about_section' );
+                    } elseif ( $current_layout == 'standard_template_product_cards' ) {
+                        get_template_part( 'sections/standard_template_product_cards' );
+                    } elseif ( $current_layout == 'additional_resources' ) {
+                        get_template_part( 'sections/standard_template_additional_resources' );
+                    }
 
-                if ( $current_layout == 'ad' ) {
-                    get_template_part( 'sections/standard_template_ad' );
-                } elseif ( $current_layout == 'about_section' ) {
-                    get_template_part( 'sections/standard_template_about_section' );
-                } elseif ( $current_layout == 'standard_template_product_cards' ) {
-                    get_template_part( 'sections/standard_template_product_cards' );
-                } elseif ( $current_layout == 'additional_resources' ) {
-                    get_template_part( 'sections/standard_template_additional_resources' );
                 }
+            ?>
 
-            }
-        ?>
+        </div>
+        
+    <?php } else {
+        echo 'no rows';
+    } ?>
 
-    </div>
+
+</div>
+
+
+
+<?php
     
-<?php } else {
-    echo 'no rows';
-}
-
-
-
-    
-
- 
-
 get_template_part( 'parts/js_footer' );
 
 //test
