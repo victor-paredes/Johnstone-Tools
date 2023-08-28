48500d89935b538066232431bbd669b4ebc9aba7
Victor Paredes <victor@paredescreative.com>
2023-08-28 07:11:42 -0700

    updating

--- templates/js_landing_page_template.php
+++ templates/js_landing_page_template.php
@@ -35,21 +35,6 @@ get_template_part( 'parts/js_header' ); ?>
             ?>
 
 
-
-            <!-- POJ block -->
-            <div class="container-fluid  my-2 columnsCollectionContainer">
-                <div class="container my-3">
-                    <div class="row">
-                        <div class="col-md">    
-                            <img class="w-100 img-fluid align-center" src="https://www.johnstonesupply.com/binaries/content/gallery/johnstonesupplypages/coleman/poj_bottom-banner-01-01.png/poj_bottom-banner-01-01.png/johnstonesupplycms%3Axl" alt="Thats the Power of Johnstone™">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
-
             
         </div> <!-- end COPY_THIS_CONTAINER_TO_BLOOMREACH -->
         
