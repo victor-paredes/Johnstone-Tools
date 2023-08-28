2e941974d2bf53208a8be0145fb3b379d5bd0917
Victor Paredes <victor@paredescreative.com>
2023-08-28 09:16:24 -0700

    updating

--- functions.php
+++ functions.php
@@ -109,9 +109,9 @@ function admin_css() {
  * @param array $post_types An array of post type names that the templates be used by
  * @return array The array of post type names that the templates be used by
  **/
-function my_cpt_post_types( $post_types ) {
-    //$post_types[] = 'landing-page';
-    //$post_types[] = 'another-type';
-    return $post_types;
-}
-add_filter( 'cpt_post_types', 'my_cpt_post_types' );
+// function my_cpt_post_types( $post_types ) {
+//     $post_types[] = 'landing-page';
+//     $post_types[] = 'another-type';
+//     return $post_types;
+// }
+// add_filter( 'cpt_post_types', 'my_cpt_post_types' );
