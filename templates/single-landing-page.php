5b745e52809d9873d3cd8a0ee14fc2d2d3ddcd49
Victor Paredes <victor@paredescreative.com>
2023-08-29 08:02:56 -0700

    updating

--- templates/single-landing-page.php
+++ templates/single-landing-page.php
@@ -5,12 +5,6 @@ Template Post Type: landing-page
 */
 
 // echo the_content(); ?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<?php wp_head(); ?>
-</head>
 
 <?php wp_head(); ?>
 
