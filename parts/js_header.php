8c689fbe5044f2b0ed9a9b10a86722d911e24892
Victor Paredes <victor@paredescreative.com>
2023-10-24 09:18:36 -0700

    updating

--- parts/js_header.php
+++ parts/js_header.php
@@ -15,9 +15,9 @@
         <div class="" style="position: absolute;">
             test
         </div>
-        <div class="">
+        
             <img class="js_header_image" src="http://victorpared.es/js/wp-content/uploads/2023/08/johnstone_header_v1.png">
-        </div>
+      
 
         
 
