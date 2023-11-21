ae9eec502dc7f338a521bf1f9895cb294970b4c3
Victor Paredes <victor@paredescreative.com>
2023-11-21 12:09:36 -0800

    adding a2l template

--- templates/single-a2l-equipment.php
+++ templates/single-a2l-equipment.php
@@ -66,16 +66,16 @@ get_template_part( 'parts/a2l_header' ); ?>
 					The Environmental Protection Agency has issued a new rule restricting the use of High GWP refrigerants (HFCs) in HVAC and Refrigerant Equipment. This means that the current refrigerant is being phased out and replaced with two new more globally friendly substitutes. Along with the new refrigerant changes your equipment will also experience a change. Most important of the changes is there is a no sell-through date, for all regions, of current R-410A HVAC equipment. Additionally, are three important aspects that you will need to know about this new rule. 
 
 					<ol>
-					<li>
-						Refrigerant Changes-Low GWP Systems and A2L Refrigerant
-					</li>	
-					<li>
-						Categories-Products, Systems, Components 
-					</li>
-					<li>
-						Equipment Mitigation Requirements-No sell through R410A systems
-					</li>
-				</ol>
+						<li>
+							Refrigerant Changes-Low GWP Systems and A2L Refrigerant
+						</li>	
+						<li>
+							Categories-Products, Systems, Components 
+						</li>
+						<li>
+							Equipment Mitigation Requirements-No sell through R410A systems
+						</li>
+					</ol>
 				</p>
 				
 
