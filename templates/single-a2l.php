96d1f23fe798a12d783f74e727d4b873ab3f1826
Victor Paredes <victor@paredescreative.com>
2023-11-22 12:33:33 -0800

    adding a2l template

--- templates/single-a2l.php
+++ templates/single-a2l.php
@@ -95,6 +95,7 @@ get_template_part( 'parts/a2l_header' ); ?>
 			<div class="col-12 pt-3 text-center">
 				<h1 class="text-light font-weight-bold">Are you ready?</h1>
 				<p class="text-light">All HVAC systems must be SEER2 certified in:</p>
+				<p>.</p>
 
 				<div id="countDown" class="row justify-content-center text-light mt-3">
 					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
