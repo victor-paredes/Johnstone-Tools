8d413c32d187e1d52b626214d45de8de798b85d6
Victor Paredes <victor@paredescreative.com>
2023-11-22 12:26:59 -0800

    adding a2l template

--- templates/single-a2l.php
+++ templates/single-a2l.php
@@ -92,6 +92,33 @@ get_template_part( 'parts/a2l_header' ); ?>
 			</div>
 
 
+			<div class="col-12 pt-5 text-center">
+				<h1 class="text-yellow font-weight-bold">Are you ready?</h1>
+				<p class="text-light">All HVAC systems must be SEER2 certified in:</p>
+				<div id="countDown" class="row justify-content-center text-light">
+					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
+						<div class="bg-light border-radius-sm shadow">
+							<span class="days d-block text-darkblue text-4rem font-weight-bold">258</span>
+							<strong class="d-block mt-n3 pb-3 text-secondary" style="color:#5A5B64;">Days</strong>
+						</div>
+					</div>
+					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
+						<div class="bg-light border-radius-sm shadow">
+							<span class="hours d-block text-darkblue text-4rem font-weight-bold">15</span>
+							<strong class="d-block mt-n3 pb-3 text-secondary">Hours</strong>
+						</div>
+					</div>
+					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
+						<div class="bg-light border-radius-sm shadow">
+							<span class="minutes d-block text-darkblue text-4rem font-weight-bold">28</span>
+							<strong class="d-block mt-n3 pb-3 text-secondary">Minutes</strong>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
 
 			<div class="container-fluid bg-white">
 				<div class="container">
