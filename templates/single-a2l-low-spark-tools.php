1121ed6d83b6f9dbb0f30285ba47d6eab645b91a
Victor Paredes <victor@paredescreative.com>
2023-11-22 11:16:41 -0800

    adding a2l template

---
+++ templates/single-a2l-low-spark-tools.php
@@ -0,0 +1,399 @@
+<?php
+/*
+Template Name: A2L Equipment
+Template Post Type: landing-page
+*/
+
+// echo the_content(); ?>
+
+<?php wp_head(); ?>
+
+<?php 
+get_template_part( 'parts/a2l_header' ); ?>
+
+
+<div class="">
+
+	<div id="COPY_THIS_CONTAINER_TO_BLOOMREACH">
+
+
+            
+        
+
+
+
+
+
+
+
+		<div class="container-fluid bg-darkblue-img mb-n5">
+
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<nav class="navbar navbar-expand-lg navbar-dark pl-0" id="mainNav">
+							<a class="navbar-brand" href="https://victorpared.es/js/landing-page/a2l-landing-page/">
+								<img width="250px" src="https://victorpared.es/js/wp-content/uploads/2023/11/johnstone_logo.png">
+							</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"
+								<span class="navbar-toggler-icon"></span>
+							</button>
+
+							<div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+								<ul class="navbar-nav text-right text-lg-center">
+									<li class="nav-item px-4 active">
+										<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-equipment/">Equipment</a>
+									</li>
+									<li class="nav-item px-4">
+										<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-refrigerant/">Refrigerant</a>
+									</li>
+									<li class="nav-item px-4">
+										<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-low-spark-tools/">Low Spark Tools</a>
+									</li>
+								</ul>
+							</div>
+
+						</nav>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-lg-11 pt-5 pb-4">
+						<h1 class="text-light mt-5 mb-4"><strong>Low Spark Tools</strong></h1>
+				
+						<div class="header_block">
+							<!-- <h2 class="mt-6 font-weight-bold hp-find-region about-seer-2" tabindex="-1">Technology Transition Rule: </h2> -->
+							<p class="lead">
+							Lorem ipsum...
+							</p>
+						</div>
+
+				
+			
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="container-fluid bg-white">
+				<div class="container">
+					<div class="row pb-5">
+
+
+						
+
+
+ 
+
+ 
+
+
+
+
+					</div>
+				</div>
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+				<style>
+					.bg-darkblue-img {
+					background: #062655 url(https://seer2.com/img/bg-contractor-installing-thermostat.png) no-repeat center center fixed;
+					-webkit-background-size: cover;
+					-moz-background-size: cover;
+					-o-background-size: cover;
+					background-size: cover;
+				}
+				.table_wrap {
+					/* margin: 0 auto; */
+					border: solid 1px #dfdfdf;
+					border-radius: .25rem;
+					padding: 2vw;
+				}
+				.table_noexpand { 
+					border-radius: 0.25rem;
+					padding: 0 15px 0 15px;
+					width: 100%;
+				}
+				.table_noexpand td {
+					padding: 15px;
+				}
+				.table_header {
+					background-color: #041e46;
+					color: #fff;
+				}
+				.table_subheader {
+					background-color: #edf2fa;
+				}
+				.header_block * {
+					color: #fff !important;
+				}
+				.a2l_button {
+					background-color: #041e47;
+					padding: 10px 15px;
+					border-radius: 999px;
+					border: 1px solid white;
+					color: #fff;
+					font-weight: 700;
+					font-size: 1rem;
+					text-align: center;
+				}
+				.a2l_button:hover {
+					background: #0072c8;
+					text-decoration: none;
+					color: white;
+				}
+				.a2l_button div {
+					margin: 0 auto;
+				}
+				.a2l_note {
+					margin-top: 15px;
+					color: #9c9c9c;
+				}
+
+
+				@media (max-width: 767px) { 
+					.table_wrap * {
+						font-size: 10px;
+					}
+					.table_noexpand { 
+						border-radius: 0.25rem;
+					} 
+					.table_noexpand td { 
+						padding: 5px;
+					}
+				}
+
+				/* .text-light, .text-light * {
+					color: #fff !important;
+				} */
+				</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        	</div> 
+
+
+        
+
+
+
+		</div> <!-- end -->
+
+	</div><!-- end COPY_THIS_CONTAINER_TO_BLOOMREACH -->
+
+
+
+
+
+		<!-- footer -->
+
+		<div class="container-fluid bg-darkblue" style="background-color: #062655;">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 col-lg-6 py-lg-5 text-center">
+						<img id="johnstoneLogo" class="d-block mx-auto mt-5 mb-3" width="400px" src="https://victorpared.es/js/wp-content/uploads/2023/11/johnstone-supply-logo-white.png" alt="Johnstone Supply">
+						<p class="lead text-light text-1rem mb-3">At Johnstone Supply, we're proud to provide you and your team the most advanced HVAC products, tools and support.</p>
+						<a href="https://www.johnstonesupply.com/" class="btn btn-lg btn-light btn-rounded text-lightblue font-weight-bolder mb-5" role="button" target="_blank" rel="noopener">Visit our website</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+		<div class="container-fluid bg-deepblue" style="background-color: #010821;">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-6 py-2 py-md-5">
+						<p class="text-light">© <span id="currentYear">2022</span> Johnstone Supply. All Rights Reserved.</p>
+					</div>
+					<div class="col-12 col-md-6 pt-2 pb-5 py-md-5 text-left text-md-right">
+						<a href="privacy-statement.html" class="d-block d-md-inline text-light px-1 px-md-4">Privacy Statement</a>
+						<a href="terms-of-use.html" class="d-block d-md-inline text-light px-1 px-md-4">Terms of Use</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+		<br><br><br><br>
+
+		<div id="js_tools_wrap">
+			<center>
+				<button id="copyButton">
+					Copy BR Code
+				</button>
+			</center>
+		</div>
+
+		<script>
+			// copy content to clipboard
+			document.addEventListener('DOMContentLoaded', function() {
+			const copyButton = document.getElementById('copyButton');
+			const container = document.getElementById('COPY_THIS_CONTAINER_TO_BLOOMREACH');
+
+			copyButton.addEventListener('click', function() {
+				if (container) {
+				const htmlContent = container.outerHTML;
+
+				const tempTextArea = document.createElement('textarea');
+				tempTextArea.value = htmlContent;
+
+				document.body.appendChild(tempTextArea);
+				tempTextArea.select();
+
+				try {
+					const success = document.execCommand('copy');
+					if (success) {
+					console.log('Content and surrounding HTML copied to clipboard');
+					alert("😎 Code copied to clipboard!");
+					} else {
+					console.error('Copying failed');
+					}
+				} catch (err) {
+					console.error('Error copying to clipboard:', err);
+				}
+
+				document.body.removeChild(tempTextArea);
+				} else {
+				console.error('Container element not found');
+				}
+			});
+			});
+		</script>
+
+
+		<style>
+			.bg-darkblue-img {
+			background: #062655 url(https://seer2.com/img/bg-contractor-installing-thermostat.png) no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+		.table_wrap {
+			/* margin: 0 auto; */
+			border: solid 1px #dfdfdf;
+			border-radius: .25rem;
+			padding: 2vw;
+		}
+		.table_noexpand { 
+			border-radius: 0.25rem;
+			padding: 0 15px 0 15px;
+			width: 100%;
+		}
+		.table_noexpand td {
+			padding: 15px;
+		}
+		.table_header {
+			background-color: #041e46;
+			color: #fff;
+		}
+		.table_subheader {
+			background-color: #edf2fa;
+		}
+		.header_block * {
+			color: #fff !important;
+		}
+		.a2l_button {
+			background-color: #041e47;
+			padding: 10px 15px;
+			border-radius: 999px;
+			border: 1px solid white;
+			color: #fff;
+			font-weight: 700;
+			font-size: 1rem;
+			text-align: center;
+		}
+		.a2l_button:hover {
+			background: #0072c8;
+			text-decoration: none;
+			color: white;
+		}
+		.a2l_button div {
+			margin: 0 auto;
+		}
+		.a2l_note {
+			margin-top: 15px;
+			color: #9c9c9c;
+		}
+
+
+		@media (max-width: 767px) { 
+			.table_wrap * {
+				font-size: 10px;
+			}
+			.table_noexpand { 
+				border-radius: 0.25rem;
+			} 
+			.table_noexpand td { 
+				padding: 5px;
+			}
+		}
+
+		/* .text-light, .text-light * {
+			color: #fff !important;
+		} */
+			</style>
+
+
+<?php
+
+the_content();
+
+// get_template_part( 'parts/js_footer' );
+
+wp_footer();
+
