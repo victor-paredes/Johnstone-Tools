8525905565b1bc3e76cb6d6202a5364008721dc0
Victor Paredes <victor@paredescreative.com>
2023-11-21 21:05:08 -0800

    adding a2l template

--- templates/single-a2l-equipment.php
+++ templates/single-a2l-equipment.php
@@ -302,9 +302,9 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 
 
-				<div class=" row table_wrap shadow"><!-- wrap -->
+							<div class=" row table_wrap shadow mt-5"><!-- wrap -->
 
-				<img width="100%" src="https://victorpared.es/js/wp-content/uploads/2023/11/what_is_a2l_refrigerant.jpg">
+								<img width="100%" src="https://victorpared.es/js/wp-content/uploads/2023/11/what_is_a2l_refrigerant.jpg">
 								<table class="table_noexpand">
 
 									<tr class="">
