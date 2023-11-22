a0874e9182c8c588e2f19a45736a874d56ab1380
Victor Paredes <victor@paredescreative.com>
2023-11-22 12:36:07 -0800

    adding a2l template

--- templates/single-a2l.php
+++ templates/single-a2l.php
@@ -109,7 +109,7 @@ get_template_part( 'parts/a2l_header' ); ?>
 							<!-- <span class="hours d-block text-darkblue text-4rem font-weight-bold">15</span> -->
 							<strong class="d-block mt-n3 pb-3 text-secondary">Hours</strong>
 						</div>
-					</div>
+					</div> 
 					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3"> 
 						<div class="bg-light border-radius-sm shadow">
 							<!-- <span class="minutes d-block text-darkblue text-4rem font-weight-bold">28</span> --> 
