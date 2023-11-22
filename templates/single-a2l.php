9b2eac93002d368dba4d71dba16d570449f67da3
Victor Paredes <victor@paredescreative.com>
2023-11-22 11:22:19 -0800

    adding a2l template

--- templates/single-a2l.php
+++ templates/single-a2l.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: A2L
+Template Name: A2L Equipment
 Template Post Type: landing-page
 */
 
@@ -14,10 +14,7 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 <div class="">
 
-
-
-
-        <div id="COPY_THIS_CONTAINER_TO_BLOOMREACH">
+	<div id="COPY_THIS_CONTAINER_TO_BLOOMREACH">
 
 
             
@@ -29,92 +26,103 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 
 
-        <div class="container-fluid bg-darkblue-img mb-n5">
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<nav class="navbar navbar-expand-lg navbar-dark pl-0" id="mainNav">
+		<div class="container-fluid bg-darkblue-img mb-n5">
 
-				<img width="250px" src="https://victorpared.es/js/wp-content/uploads/2023/11/johnstone_logo.png">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<nav class="navbar navbar-expand-lg navbar-dark pl-0" id="mainNav">
+							<a class="navbar-brand" href="https://victorpared.es/js/landing-page/a2l-landing-page/">
+								<img width="250px" src="https://victorpared.es/js/wp-content/uploads/2023/11/johnstone_logo.png">
+							</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"
+								<span class="navbar-toggler-icon"></span>
+							</button>
 
+							<div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+								<ul class="navbar-nav text-right text-lg-center">
+									<li class="nav-item px-4 active">
+										<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-equipment/">Equipment</a>
+									</li>
+									<li class="nav-item px-4">
+										<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-refrigerant/">Refrigerant</a>
+									</li>
+									<li class="nav-item px-4">
+										<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-low-spark-tools/">Low Spark Tools</a>
+									</li>
+								</ul>
+							</div>
 
-
-				<div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-						<ul class="navbar-nav text-right text-lg-center">
-							<li class="nav-item px-4 active">
-								<a class="nav-link text-light" href="https://victorpared.es/js/landing-page/a2l-equipment/">Equipment</a>
-							</li>
-							<li class="nav-item px-4">
-								<a class="nav-link text-light" href="#">Refrigerant</a>
-							</li>
-							<li class="nav-item px-4">
-								<a class="nav-link text-light" href="#">Low Spark Tools</a> 
-							</li>
-						</ul>
+						</nav>
 					</div>
-				</nav>
+				</div>
 			</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row">
 
 
 
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-lg-11 pt-5 pb-4">
+						<h1 class="text-light mt-5 mb-4"><strong>Technology Transition Rule</strong></h1>
+				
+						<div class="header_block">
+							<!-- <h2 class="mt-6 font-weight-bold hp-find-region about-seer-2" tabindex="-1">Technology Transition Rule: </h2> -->
+							<p class="lead">
+							The Environmental Protection Agency has issued a new rule restricting the use of High GWP refrigerants (HFCs) in HVAC and Refrigerant Equipment. This means that the current refrigerant is being phased out and replaced with two new more globally friendly substitutes. Along with the new refrigerant changes your equipment will also experience a change. Most important of the changes is there is a no sell-through date, for all regions, of current R-410A HVAC equipment. Additionally, are three important aspects that you will need to know about this new rule. 
+							</p>
+						</div>
 
-			<div class="col-12 col-lg-6 mt-lg-5 mb-lg-3 pt-lg-5 pb-lg-2">
-				<h1 class="text-light mt-5 mb-4"><strong>Changes are coming to <br class="d-none d-lg-block">your region.</strong></h1>
-				<p class="lead text-light text-1rem mb-4">Lorem ipsum...</p>
+						<p class="text-light lead lead">
+						<strong>There are three important aspects of the new Technology Transition Rule:</strong>
+						</p>	
 
-
-
-
-
-				<!-- <p class="mb-2"><span class="badge badge-warning mr-2">NEW</span> <a href="about.html#inventory" class="text-light">Compliance for Existing Inventory →</a></p>
-				<p class="mb-4"><span class="badge badge-warning mr-2">NEW</span> <a href="https://web.archive.org/web/20220811051637/https://go.bluevolt.com/johnstonecontractor/s/coursedetail/386741/supplier-webinar-recording--coleman-2023-seer2-information" class="text-light" rel="noopener" target="_blank">Coleman Webinar Recording →</a></p>
-				<a href="#" id="scrollToMap" class="btn btn-lg btn-light btn-rounded text-lightblue font-weight-bolder" role="button">Find your region</a> -->
-
-
-
-
-			</div>
-			<div class="col-12 col-lg-6 mt-lg-5 mb-lg-3 pt-lg-5 pb-lg-2">
-				<div class="embed-responsive embed-responsive-16by9 mt-5">
-					<iframe class="embed-responsive-item" src="https://web.archive.org/web/20220811051637if_/https://www.youtube.com/embed/AOECX5zu7OE?rel=0" title="2023 Energy Efficiency Regulations" allowfullscreen="" data-ruffle-polyfilled=""></iframe>
+						<ol>
+							<li class="text-light lead">
+								Refrigerant Changes-Low GWP Systems and A2L Refrigerant
+							</li>
+							<li class="text-light lead">
+								Categories-Products, Systems, Components 
+							</li>
+							<li class="text-light lead">
+								Equipment Mitigation Requirements-No sell through R410A systems
+							</li>
+						</ol>
+					</div>
 				</div>
 			</div>
 
 
 
+			<div class="container-fluid bg-white">
+				<div class="container">
+					<div class="row pb-5">
 
 
-			<div class="col-12 pt-4 mt-3 text-center">
-				<h1 class="text-yellow font-weight-bold">Are you ready?</h1>
-				<p class="text-light">All HVAC systems must be SEER2 certified in:</p>
-				<div id="countDown" class="row justify-content-center text-light">
-					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
-						<div class="bg-light border-radius-sm shadow">
-							<span class="days d-block text-darkblue text-4rem font-weight-bold">143</span>
-							<strong class="d-block mt-n3 pb-3 text-secondary" style="color:#5A5B64;">Days</strong>
-						</div>
-					</div>
-					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
-						<div class="bg-light border-radius-sm shadow">
-							<span class="hours d-block text-darkblue text-4rem font-weight-bold">01</span>
-							<strong class="d-block mt-n3 pb-3 text-secondary">Hours</strong>
-						</div>
-					</div>
-					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
-						<div class="bg-light border-radius-sm shadow">
-							<span class="minutes d-block text-darkblue text-4rem font-weight-bold">42</span>
-							<strong class="d-block mt-n3 pb-3 text-secondary">Minutes</strong>
-						</div>
+						
+
+
+
+
+
+
+
+
+
+
+
+						
+ 
+
+
+
+
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
-</div>
+
+
+
+
+ 
 
 
 
@@ -126,20 +134,87 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 
 
+				<style>
+					.bg-darkblue-img {
+					background: #062655 url(https://seer2.com/img/bg-contractor-installing-thermostat.png) no-repeat center center fixed;
+					-webkit-background-size: cover;
+					-moz-background-size: cover;
+					-o-background-size: cover;
+					background-size: cover;
+				}
+				.table_wrap {
+					/* margin: 0 auto; */
+					border: solid 1px #dfdfdf;
+					border-radius: .25rem;
+					padding: 2vw;
+				}
+				.table_noexpand { 
+					border-radius: 0.25rem;
+					padding: 0 15px 0 15px;
+					width: 100%;
+				}
+				.table_noexpand td {
+					padding: 15px;
+				}
+				.table_header {
+					background-color: #041e46;
+					color: #fff;
+				}
+				.table_subheader {
+					background-color: #edf2fa;
+				}
+				.header_block * {
+					color: #fff !important;
+				}
+				.a2l_button {
+					background-color: #041e47;
+					padding: 10px 15px;
+					border-radius: 999px;
+					border: 1px solid white;
+					color: #fff;
+					font-weight: 700;
+					font-size: 1rem;
+					text-align: center;
+				}
+				.a2l_button:hover {
+					background: #0072c8;
+					text-decoration: none;
+					color: white;
+				}
+				.a2l_button div {
+					margin: 0 auto;
+				}
+				.a2l_note {
+					margin-top: 15px;
+					color: #9c9c9c;
+				}
+
+
+				@media (max-width: 767px) { 
+					.table_wrap * {
+						font-size: 10px;
+					}
+					.table_noexpand { 
+						border-radius: 0.25rem;
+					} 
+					.table_noexpand td { 
+						padding: 5px;
+					}
+				}
+
+				/* .text-light, .text-light * {
+					color: #fff !important;
+				} */
+				</style>
 
 
 
 
 
-<style>
-	.bg-darkblue-img {
-    background: #062655 url(https://seer2.com/img/bg-darkblue-pattern.jpg) no-repeat center center fixed;
-    -webkit-background-size: cover;
-    -moz-background-size: cover;
-    -o-background-size: cover;
-    background-size: cover;
-}
-	</style>
+
+
+
+
 
 
 
@@ -162,59 +237,50 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 
             
-        </div> <!-- end COPY_THIS_CONTAINER_TO_BLOOMREACH -->
+        	</div> 
 
 
         
 
 
 
-</div> <!-- end -->
+		</div> <!-- end -->
 
-
-<br><br><br><br>
-
-
+	</div><!-- end COPY_THIS_CONTAINER_TO_BLOOMREACH -->
 
 
 
 
 
+		<!-- footer -->
 
-
-
-
-
-
-<!-- footer -->
-
-<div class="container-fluid bg-darkblue" style="background-color: #062655;">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-12 col-lg-6 py-lg-5 text-center">
-				<img id="johnstoneLogo" class="d-block mx-auto mt-5 mb-3" width="400px" src="https://victorpared.es/js/wp-content/uploads/2023/11/johnstone-supply-logo-white.png" alt="Johnstone Supply">
-				<p class="lead text-light text-1rem mb-3">At Johnstone Supply, we're proud to provide you and your team the most advanced HVAC products, tools and support.</p>
-				<a href="https://www.johnstonesupply.com/" class="btn btn-lg btn-light btn-rounded text-lightblue font-weight-bolder mb-5" role="button" target="_blank" rel="noopener">Visit our website</a>
+		<div class="container-fluid bg-darkblue" style="background-color: #062655;">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 col-lg-6 py-lg-5 text-center">
+						<img id="johnstoneLogo" class="d-block mx-auto mt-5 mb-3" width="400px" src="https://victorpared.es/js/wp-content/uploads/2023/11/johnstone-supply-logo-white.png" alt="Johnstone Supply">
+						<p class="lead text-light text-1rem mb-3">At Johnstone Supply, we're proud to provide you and your team the most advanced HVAC products, tools and support.</p>
+						<a href="https://www.johnstonesupply.com/" class="btn btn-lg btn-light btn-rounded text-lightblue font-weight-bolder mb-5" role="button" target="_blank" rel="noopener">Visit our website</a>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
 
 
 
-<div class="container-fluid bg-deepblue" style="background-color: #010821;">
-	<div class="container">
-		<div class="row">
-			<div class="col-12 col-md-6 py-2 py-md-5">
-				<p class="text-light">© <span id="currentYear">2022</span> Johnstone Supply. All Rights Reserved.</p>
-			</div>
-			<div class="col-12 col-md-6 pt-2 pb-5 py-md-5 text-left text-md-right">
-				<a href="privacy-statement.html" class="d-block d-md-inline text-light px-1 px-md-4">Privacy Statement</a>
-				<a href="terms-of-use.html" class="d-block d-md-inline text-light px-1 px-md-4">Terms of Use</a>
+		<div class="container-fluid bg-deepblue" style="background-color: #010821;">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-6 py-2 py-md-5">
+						<p class="text-light">© <span id="currentYear">2022</span> Johnstone Supply. All Rights Reserved.</p>
+					</div>
+					<div class="col-12 col-md-6 pt-2 pb-5 py-md-5 text-left text-md-right">
+						<a href="privacy-statement.html" class="d-block d-md-inline text-light px-1 px-md-4">Privacy Statement</a>
+						<a href="terms-of-use.html" class="d-block d-md-inline text-light px-1 px-md-4">Terms of Use</a>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
 
 
 
@@ -223,64 +289,125 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 
 
+		<br><br><br><br>
+
+		<div id="js_tools_wrap">
+			<center>
+				<button id="copyButton">
+					Copy BR Code
+				</button>
+			</center>
+		</div>
+
+		<script>
+			// copy content to clipboard
+			document.addEventListener('DOMContentLoaded', function() {
+			const copyButton = document.getElementById('copyButton');
+			const container = document.getElementById('COPY_THIS_CONTAINER_TO_BLOOMREACH');
+
+			copyButton.addEventListener('click', function() {
+				if (container) {
+				const htmlContent = container.outerHTML;
+
+				const tempTextArea = document.createElement('textarea');
+				tempTextArea.value = htmlContent;
+
+				document.body.appendChild(tempTextArea);
+				tempTextArea.select();
+
+				try {
+					const success = document.execCommand('copy');
+					if (success) {
+					console.log('Content and surrounding HTML copied to clipboard');
+					alert("😎 Code copied to clipboard!");
+					} else {
+					console.error('Copying failed');
+					}
+				} catch (err) {
+					console.error('Error copying to clipboard:', err);
+				}
+
+				document.body.removeChild(tempTextArea);
+				} else {
+				console.error('Container element not found');
+				}
+			});
+			});
+		</script>
 
 
+		<style>
+			.bg-darkblue-img {
+			background: #062655 url(https://seer2.com/img/bg-contractor-installing-thermostat.png) no-repeat center center fixed;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+		}
+		.table_wrap {
+			/* margin: 0 auto; */
+			border: solid 1px #dfdfdf;
+			border-radius: .25rem;
+			padding: 2vw;
+		}
+		.table_noexpand { 
+			border-radius: 0.25rem;
+			padding: 0 15px 0 15px;
+			width: 100%;
+		}
+		.table_noexpand td {
+			padding: 15px;
+		}
+		.table_header {
+			background-color: #041e46;
+			color: #fff;
+		}
+		.table_subheader {
+			background-color: #edf2fa;
+		}
+		.header_block * {
+			color: #fff !important;
+		}
+		.a2l_button {
+			background-color: #041e47;
+			padding: 10px 15px;
+			border-radius: 999px;
+			border: 1px solid white;
+			color: #fff;
+			font-weight: 700;
+			font-size: 1rem;
+			text-align: center;
+		}
+		.a2l_button:hover {
+			background: #0072c8;
+			text-decoration: none;
+			color: white;
+		}
+		.a2l_button div {
+			margin: 0 auto;
+		}
+		.a2l_note {
+			margin-top: 15px;
+			color: #9c9c9c;
+		}
 
 
+		@media (max-width: 767px) { 
+			.table_wrap * {
+				font-size: 10px;
+			}
+			.table_noexpand { 
+				border-radius: 0.25rem;
+			} 
+			.table_noexpand td { 
+				padding: 5px;
+			}
+		}
 
-
-
-
-
-<div id="js_tools_wrap">
-    <center>
-        <button id="copyButton">
-            Copy BR Code
-        </button>
-    </center>
-</div>
-
-<script>
-    // copy content to clipboard
-    document.addEventListener('DOMContentLoaded', function() {
-    const copyButton = document.getElementById('copyButton');
-    const container = document.getElementById('COPY_THIS_CONTAINER_TO_BLOOMREACH');
-
-    copyButton.addEventListener('click', function() {
-        if (container) {
-        const htmlContent = container.outerHTML;
-
-        const tempTextArea = document.createElement('textarea');
-        tempTextArea.value = htmlContent;
-
-        document.body.appendChild(tempTextArea);
-        tempTextArea.select();
-
-        try {
-            const success = document.execCommand('copy');
-            if (success) {
-            console.log('Content and surrounding HTML copied to clipboard');
-            alert("😎 Code copied to clipboard!");
-            } else {
-            console.error('Copying failed');
-            }
-        } catch (err) {
-            console.error('Error copying to clipboard:', err);
-        }
-
-        document.body.removeChild(tempTextArea);
-        } else {
-        console.error('Container element not found');
-        }
-    });
-    });
-</script>
-
-
-
-
-
-
+		/* .text-light, .text-light * {
+			color: #fff !important;
+		} */
+			</style>
 
 
 <?php
