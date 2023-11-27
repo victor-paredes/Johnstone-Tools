ce5baabc5c4911e5ceb73ec169ace2a4f1ce5c2e
Victor Paredes <victor@paredescreative.com>
2023-11-27 13:33:21 -0800

    adding counter

--- templates/single-a2l.php
+++ templates/single-a2l.php
@@ -92,29 +92,35 @@ get_template_part( 'parts/a2l_header' ); ?>
 			</div>
 
 
-			<div class="col-12 pt-4 mt-3 text-center">
-	<h1 class="text-yellow font-weight-bold"></h1>
-	<div id="countDown" class="row justify-content-center text-light">
-		<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
-			<div class="bg-light border-radius-sm shadow">
-				<span class="days d-block text-darkblue text-4rem font-weight-bold">&nbsp;</span>
-				<strong class="d-block mt-n3 pb-3 text-secondary" style="color:#5A5B64;">Days</strong>
+			<div class="col-12 pt-3 text-center">
+				<h1 class="text-light font-weight-bold">Are you ready?</h1>
+				<p class="text-light">All HVAC systems must be SEER2 certified in:</p>
+				<p>&nbsp;</p>
+
+				<div id="countDown" class="row justify-content-center text-light mt-3">
+					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
+						<div class="bg-light border-radius-sm shadow">
+							<!-- <span class="days d-block text-darkblue text-4rem font-weight-bold">258</span> -->
+							<strong class="d-block mt-n3 pb-3 text-secondary" style="color:#5A5B64;">Days</strong>
+						</div>
+					</div>
+					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
+						<div class="bg-light border-radius-sm shadow">
+							<!-- <span class="hours d-block text-darkblue text-4rem font-weight-bold">15</span> -->
+							<strong class="d-block mt-n3 pb-3 text-secondary">Hours</strong>
+						</div>
+					</div> 
+					<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3"> 
+						<div class="bg-light border-radius-sm shadow">
+							<!-- <span class="minutes d-block text-darkblue text-4rem font-weight-bold">28</span> --> 
+							<strong class="d-block mt-n3 pb-3 text-secondary">Minutes</strong>
+						</div>
+					</div>
+				</div>
+
+				<p>&nbsp;</p>
+
 			</div>
-		</div>
-		<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
-			<div class="bg-light border-radius-sm shadow">
-				<span class="hours d-block text-darkblue text-4rem font-weight-bold">&nbsp;</span>
-				<strong class="d-block mt-n3 pb-3 text-secondary">Hours</strong>
-			</div>
-		</div>
-		<div class="col-4 col-md-3 col-lg-2 px-1 px-md-3">
-			<div class="bg-light border-radius-sm shadow">
-				<span class="minutes d-block text-darkblue text-4rem font-weight-bold">&nbsp;</span>
-				<strong class="d-block mt-n3 pb-3 text-secondary">Minutes</strong>
-			</div>
-		</div>
-	</div>
-</div>
 
 
 
@@ -237,50 +243,50 @@ get_template_part( 'parts/a2l_header' ); ?>
 
 
 
+				<script>
+				const deadline = '2023-11-28';
+				initializeClock('countDown', deadline);
+
+				function getTimeRemaining(endtime) {
+					const total = Date.parse(endtime) - Date.parse(new Date());
+					const seconds = Math.floor((total / 1000) % 60);
+					const minutes = Math.floor((total / 1000 / 60) % 60);
+					const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+					const days = Math.floor(total / (1000 * 60 * 60 * 24));
+					return {
+						total,
+						days,
+						hours,
+						minutes,
+						seconds
+					};
+				}
+				function initializeClock(id, endtime) {
+					const clock = document.getElementById(id);
+					const daysSpan = clock.querySelector('.days');
+					const hoursSpan = clock.querySelector('.hours');
+					const minutesSpan = clock.querySelector('.minutes');
+					//const secondsSpan = clock.querySelector('.seconds');
+					function updateClock() {
+						const t = getTimeRemaining(endtime);
+
+						daysSpan.innerHTML = t.days;
+						hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+						minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+						//secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+						if (t.total <= 0) {
+							clearInterval(timeinterval);
+						}
+					}
+					updateClock();
+					const timeinterval = setInterval(updateClock, 1000);
+
+				}
+				</script>
 
 
 
-<script>
-const deadline = '2023-11-28';
-initializeClock('countDown', deadline);
-
-function getTimeRemaining(endtime) {
-	const total = Date.parse(endtime) - Date.parse(new Date());
-	const seconds = Math.floor((total / 1000) % 60);
-	const minutes = Math.floor((total / 1000 / 60) % 60);
-	const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-	const days = Math.floor(total / (1000 * 60 * 60 * 24));
-	return {
-		total,
-		days,
-		hours,
-		minutes,
-		seconds
-	};
-}
-function initializeClock(id, endtime) {
-	const clock = document.getElementById(id);
-	const daysSpan = clock.querySelector('.days');
-	const hoursSpan = clock.querySelector('.hours');
-	const minutesSpan = clock.querySelector('.minutes');
-	//const secondsSpan = clock.querySelector('.seconds');
-	function updateClock() {
-		const t = getTimeRemaining(endtime);
-
-		daysSpan.innerHTML = t.days;
-		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-		//secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-		if (t.total <= 0) {
-			clearInterval(timeinterval);
-		}
-	}
-	updateClock();
-	const timeinterval = setInterval(updateClock, 1000);
-
-}
-</script>
 
 
 
