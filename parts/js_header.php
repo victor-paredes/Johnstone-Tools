4d7f9d8a5ceef040105f0f242ccb368050bc9f5e
Victor Paredes <victor@paredescreative.com>
2023-12-13 08:25:05 -0800

    updating

--- parts/js_header.php
+++ parts/js_header.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>JS - <?php wp_title(''); ?></title>
+    <title><?php wp_title(''); ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/styles/johnstone.css">
 </head>
