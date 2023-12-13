c51fa1e099a2d7cfe454e594fe82ca60da29c0f2
Victor Paredes <victor@paredescreative.com>
2023-12-13 13:21:41 -0800

    updating

--- sections/standard_template_accordion.php
+++ sections/standard_template_accordion.php
@@ -12,11 +12,35 @@
                 
                 <div class="" style="background-color: transparent; border: initial;">
               
-                            <b><?php the_sub_field('accordion_item'); ?></b>
+                            
                             <br>
-                            <?php the_sub_field('accordion_item_content'); ?>
+                            
 
                 </div> <!-- END .card -->
+
+
+
+
+
+                <div class="left-menu">
+                    <div class="accordion">
+                        <div class="section">
+                            <input type="radio" name="accordion-1" id="section-1" checked="checked"/>
+                            <label for="section-1">
+                                <span>
+                                    <b><?php the_sub_field('accordion_item'); ?></b>
+                                </span>
+                            </label>
+                            <div class="content">
+                                <ul>
+                                    <li>
+                                        <?php the_sub_field('accordion_item_content'); ?>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
 
 
 
@@ -26,20 +50,6 @@
 
 
             
-                
-             
-                    
-
-
-
-
-
-
-
-
-
-
-
 
 
 </div> <!-- standard_template_product_cards_wrap -->
@@ -48,6 +58,127 @@
 
 
 <style>
+    .left-menu {
+  background: #222;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+}
+
+.accordion {
+  color: #FFF;
+  width: 100%;
+}
+.accordion .section {
+  width: 100%;
+}
+.accordion .section input[type='radio'] {
+  display: none;
+}
+.accordion .section input[type='radio']:checked + label {
+  background: #363636;
+}
+.accordion .section input[type='radio']:checked + label:before {
+  content: " ";
+  position: absolute;
+  border-left: 3px solid #21CCFC;
+  height: 100%;
+  left: 0;
+}
+.accordion .section input[type='radio']:checked ~ .content {
+  max-height: 300px;
+  opacity: 1;
+  z-index: 10;
+  overflow-y: auto;
+}
+.accordion .section label {
+  position: relative;
+  cursor: pointer;
+  padding: 10px 20px;
+  display: table;
+  background: #222222;
+  width: 100%;
+  -webkit-transition: background 0.3s ease-in-out;
+  -moz-transition: background 0.3s ease-in-out;
+  -ms-transition: background 0.3s ease-in-out;
+  transition: background 0.3s ease-in-out;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+}
+.accordion .section label:before {
+  content: " ";
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 1px;
+  border-top: 1px solid #363636;
+}
+.accordion .section label:hover {
+  background: #363636;
+}
+.accordion .section label span {
+  display: table-cell;
+  vertical-align: middle;
+}
+.accordion .section:last-of-type {
+  border-bottom: 1px solid #363636;
+}
+.accordion .section .content {
+  max-height: 0;
+  -webkit-transition: all 0.4s;
+  -moz-transition: all 0.4s;
+  -ms-transition: all 0.4s;
+  transition: all 0.4s;
+  opacity: 0;
+  position: relative;
+  overflow-y: hidden;
+}
+
+*, *:before, *:after {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.accordion {
+  font-size: 14px;
+}
+.accordion .section .content {
+  padding: 0 15px;
+}
+.accordion .section input[type='radio'] {
+  display: none;
+}
+.accordion .section input[type='radio']:checked ~ .content {
+  padding: 15px;
+}
+
+ul {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+ul li {
+  padding: 10px;
+}
+ul li i {
+  font-size: 13px;
+  width: 15px;
+  margin-right: 15px;
+}
+ul li:hover {
+  cursor: pointer;
+}
+ul li:hover i {
+  color: #21CCFC;
+}
+
 </style>
 
 
--- sections/standard_template_additional_resources.php
+++ sections/standard_template_additional_resources.php
@@ -10,35 +10,7 @@
 
 
 
-                    <style type="text/css">
-                        .contain_js {
-                            display: flex;
-                            justify-content: center;
-                            align-items: center;
-                            padding: 5px, 30px;
-                            margin: 30px;
-                            text-align: center;
-                        }
-                        .addl-resources {
-                            display: inline-block;
-                            justify-content: center;
-                            align-items: center;
-                            border-left: 1px solid #fff;
-                            width: 25%;
-                            height: 90px;
-                            padding-top: 12pt;
-                            padding-bottom: 5pt;
-                            padding-right: 10px;
-                            padding-left: 10px;
-                            margin: 0pt;
-                            text-align: center;
-                            background-color: #004990;
-                        }
-                        a.js {
-                            color: #fff;
-                            text-decoration: none;
-                        }
-                    </style>
+                   
 
 
 
@@ -82,3 +54,34 @@
         </div>
     </div>
 </div>
+
+
+<style type="text/css">
+                        .contain_js {
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            padding: 5px, 30px;
+                            margin: 30px;
+                            text-align: center;
+                        }
+                        .addl-resources {
+                            display: inline-block;
+                            justify-content: center;
+                            align-items: center;
+                            border-left: 1px solid #fff;
+                            width: 25%;
+                            height: 90px;
+                            padding-top: 12pt;
+                            padding-bottom: 5pt;
+                            padding-right: 10px;
+                            padding-left: 10px;
+                            margin: 0pt;
+                            text-align: center;
+                            background-color: #004990;
+                        }
+                        a.js {
+                            color: #fff;
+                            text-decoration: none;
+                        }
+                    </style>
