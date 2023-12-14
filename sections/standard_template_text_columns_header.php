0a4f74fe28b746e09c9a0d1e13f786aca1fee47b
Victor Paredes <victor@paredescreative.com>
2023-12-14 14:29:43 -0800

    adding a2l template

--- sections/standard_template_text_columns_header.php
+++ sections/standard_template_text_columns_header.php
@@ -52,7 +52,7 @@
         height: 1px;
         border: 0;
         border-top: 1px solid #ccc;
-        margin: 1em 0;
+        margin: 1em 0; 
         padding: 0;
     }
     .text_columns_header_textcard {
