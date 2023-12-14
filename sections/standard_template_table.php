de8cc01e43102133003e53ffcfbf7dad051a9c88
Victor Paredes <victor@paredescreative.com>
2023-12-14 12:46:11 -0800

    adding a2l template

--- sections/standard_template_table.php
+++ sections/standard_template_table.php
@@ -1,100 +1,116 @@
 <div class="col-12 pt-3 text-left"> <!-- start table -->
 			
+    <div class=" row table_wrap shadow"><!-- wrap -->
+        <table class="table_noexpand">
+
+            <tr class="">
+                <td class=""> 
+                    <strong></strong>
+                </td>
+                <td class="table_header border-left">  
+                    <strong>R-454B </strong>
+                </td>
+                <td class="table_header">
+                    <strong>R-32</strong>
+                </td>
+                <td class="table_header">
+                    <strong>R-410A</strong>
+                </td>
+            </tr>
+            
+                
+            <tr class="">
+                <td class="border-bottom border-right table_subheader">
+                    <strong>GWP</strong>
+                </td>
+                <td class="border-bottom border-right table_subheader">
+                    <strong>466</strong>
+                    </td>
+
+                <td class="border-bottom border-right table_subheader">
+                    <strong>675</strong>
+                    </td>
+                <td class="border-bottom table_subheader">
+                    <strong>2088</strong>
+                    </td>
+            </tr>
+            
+
+            <tr class="">
+                <td class="border-bottom border-right">
+                    Discarge Temperatures
+                </td>
+                <td class="border-bottom border-right">
+                Lower Discharge Temperatures
+                </td>
+
+                <td class="border-bottom border-right">
+                Higher Discharge Temperatures
+                </td>
+                <td class="border-bottom">
+                Lower Discharge Temperatures
+                </td>
+            </tr>
+
+
+            <tr class="">
+                <td class="border-bottom border-right">
+                    Glide
+                </td>
+                <td class="border-bottom border-right">
+                Little to no glide
+                </td>
+
+                <td class="border-bottom border-right">
+                Little to no glide
+                </td>
+                <td class="border-bottom">
+                No Glide
+                </td>
+            </tr>
+
+
+            <tr class="">
+                <td class="border-right">
+                    Formula
+                </td>
+                <td class="border-right">
+                68.9% R-32/31.1% R-1234yf
+                </td>
+
+                <td class="border-right">
+                100% R-32
+                </td>
+                <td class="">
+                50% R-32/50% R-125 
+                </td>
+            </tr>
+        </table>
+    </div> <!-- end wrap -->
+
+</div><!-- end table -->
 
 
 
-	
 
 
-
-							<div class=" row table_wrap shadow"><!-- wrap -->
-								<table class="table_noexpand">
-
-									<tr class="">
-										<td class=""> 
-											<strong></strong>
-										</td>
-										<td class="table_header border-left">  
-											<strong>R-454B </strong>
-										</td>
-										<td class="table_header">
-											<strong>R-32</strong>
-										</td>
-										<td class="table_header">
-											<strong>R-410A</strong>
-										</td>
-									</tr>
-									
-										
-									<tr class="">
-										<td class="border-bottom border-right table_subheader">
-											<strong>GWP</strong>
-										</td>
-										<td class="border-bottom border-right table_subheader">
-											<strong>466</strong>
-											</td>
-
-										<td class="border-bottom border-right table_subheader">
-											<strong>675</strong>
-											</td>
-										<td class="border-bottom table_subheader">
-											<strong>2088</strong>
-											</td>
-									</tr>
-									
-
-									<tr class="">
-										<td class="border-bottom border-right">
-											Discarge Temperatures
-										</td>
-										<td class="border-bottom border-right">
-										Lower Discharge Temperatures
-										</td>
-
-										<td class="border-bottom border-right">
-										Higher Discharge Temperatures
-										</td>
-										<td class="border-bottom">
-										Lower Discharge Temperatures
-										</td>
-									</tr>
-
-
-									<tr class="">
-										<td class="border-bottom border-right">
-											Glide
-										</td>
-										<td class="border-bottom border-right">
-										Little to no glide
-										</td>
-
-										<td class="border-bottom border-right">
-										Little to no glide
-										</td>
-										<td class="border-bottom">
-										No Glide
-										</td>
-									</tr>
-
-
-									<tr class="">
-										<td class="border-right">
-											Formula
-										</td>
-										<td class="border-right">
-										68.9% R-32/31.1% R-1234yf
-										</td>
-
-										<td class="border-right">
-										100% R-32
-										</td>
-										<td class="">
-										50% R-32/50% R-125 
-										</td>
-									</tr>
-								</table>
-							</div> <!-- end wrap -->
-
-
-
-						</div><!-- end table -->
+<style>
+    .table_wrap {
+        /* margin: 0 auto; */
+        border: solid 1px #dfdfdf;
+        border-radius: .25rem;
+        padding: 2vw;
+    }
+    .table_noexpand { 
+        border-radius: 0.25rem;
+        padding: 0 15px 0 15px;
+        width: 100%;
+    }
+    .table_noexpand td {
+        padding: 15px;
+    }
+    .table_header {
+        background-color: #041e46;
+        color: #fff;
+    }
+</style>
