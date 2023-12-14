364a86d1d2dedd28a7785b960ca2e0f81c1e1ef0
Victor Paredes <victor@paredescreative.com>
2023-12-14 14:34:28 -0800

    adding a2l template

--- sections/standard_template_text_columns_header.php
+++ sections/standard_template_text_columns_header.php
@@ -52,7 +52,7 @@
         height: 1px;
         width: 100%;
         border: 0;
-        border-top: 1px solid linear-gradient(24deg, rgba(63,153,243,1) 0%, rgba(255,255,255,0) 100%);;
+        border-top: 1px solid linear-gradient(to right, rgba(63,153,243,1) 0%, rgba(255,255,255,0) 100%);;
         margin: 1em 0; 
         padding: 0;
     }
