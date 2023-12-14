1d16f49ba6194a21b24df2aea877368e57f08fe9
Victor Paredes <victor@paredescreative.com>
2023-12-14 14:29:23 -0800

    adding a2l template

--- sections/standard_template_text_columns_header.php
+++ sections/standard_template_text_columns_header.php
@@ -48,7 +48,12 @@
         color: #25aff3 !important;
     }
     .text_columns_header_wrap hr {
-        border-top: 2px solid #fff !important;
+        display: block;
+        height: 1px;
+        border: 0;
+        border-top: 1px solid #ccc;
+        margin: 1em 0;
+        padding: 0;
     }
     .text_columns_header_textcard {
         display: flex;
