96d9dff5c290d1ba66a0e234f6ae6eb6b00311bd
Victor Paredes <victor@paredescreative.com>
2023-12-14 09:01:23 -0800

    adding a2l template

---
+++ sections/standard_template_text_columns_header.php
@@ -0,0 +1,50 @@
+
+<div class="" style="margin-top: 25px;">
+    <div class="card-deck">
+
+
+
+
+        <?php
+            while( have_rows('text_column_content') ){ 
+                the_row(); 
+        ?>
+     
+                
+                <div class="card" style="background-color: transparent; border: initial;">
+              
+                            <?php the_sub_field('column_copy'); ?>
+
+                </div> <!-- END .card -->
+
+
+
+        <?php
+            } // END while( have_rows('copy') )
+        ?>
+
+
+            
+                
+             
+                    
+
+
+
+
+
+
+
+<style>
+</style>
+
+
+
+
+
+
+
+    </div> <!-- card-deck -->
+</div> <!-- standard_template_product_cards_wrap -->
+
+
