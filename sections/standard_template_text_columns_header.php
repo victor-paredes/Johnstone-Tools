78c864ddd698f43fed16e420b725ebc2ea39c567
Victor Paredes <victor@paredescreative.com>
2023-12-14 10:19:38 -0800

    adding a2l template

--- sections/standard_template_text_columns_header.php
+++ sections/standard_template_text_columns_header.php
@@ -8,7 +8,7 @@
      
                 
                 <div class="card" style="background-color: transparent; border: initial;">
-                    <div class="text_columns_header_textcard p-5">
+                    <div class="text_columns_header_textcard pl-5 py-5">
                         <div class="smalltext w-100" style="padding-top: 30px; text-transform: uppercase;">
                             <?php the_sub_field('subheader'); ?>
                         </div>
