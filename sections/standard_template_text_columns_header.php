50c38633e85bcfd1b522c47409e96fcc513cf32e
Victor Paredes <victor@paredescreative.com>
2023-12-14 09:54:32 -0800

    adding a2l template

--- sections/standard_template_text_columns_header.php
+++ sections/standard_template_text_columns_header.php
@@ -17,7 +17,7 @@
                 </div> <!-- END .card -->
 
                 <div class="card" style="background-color: transparent; border: initial;">
-                    <div class="pr-5 py-5">
+                    <div class="py-5">
                         <img src="<?php the_sub_field('featured_image'); ?>">
                     </div>
 
