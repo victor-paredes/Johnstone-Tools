78932e2ace6cbf50ee395f99cc0c992ee580c512
Victor Paredes <victor@paredescreative.com>
2023-12-15 09:31:47 -0800

    adding a2l template

--- templates/single-landing-page.php
+++ templates/single-landing-page.php
@@ -70,12 +70,12 @@ get_template_part( 'parts/js_header' ); ?>
 
 
                 @media screen and (max-width: 576px) {
-                    .narrow_only {
+                    /* .narrow_only {
                         display: initial;
                     }
                     .wide_only {
                         display: none;
-                    }
+                    } */
                     .text_columns_header_textcard {
                         padding: 0 30px !important;
                     }
@@ -84,7 +84,12 @@ get_template_part( 'parts/js_header' ); ?>
                     } 
                 }
                 @media screen and (max-width: 992px) {
-
+                    .narrow_only {
+                        display: initial;
+                    }
+                    .wide_only {
+                        display: none;
+                    }
                 }
                 /* @media screen and (min-width: 1200px) {
 
