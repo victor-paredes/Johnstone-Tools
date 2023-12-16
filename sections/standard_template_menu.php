9e464aa2b8e255a7eb8630e866fb67cdd732b4da
Victor Paredes <victor@paredescreative.com>
2023-12-15 17:43:22 -0800

    adding a2l template

--- sections/standard_template_menu.php
+++ sections/standard_template_menu.php
@@ -24,7 +24,7 @@
             
             
 
-            <a class="menu_item" href="<?php the_sub_field('link'); ?>">
+            <a class="menu_item <?php the_sub_field('menu_label'); ?>" href="<?php the_sub_field('link'); ?>">
                         <?php the_sub_field('label'); ?>
             </a>
                
