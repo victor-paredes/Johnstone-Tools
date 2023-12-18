38b30a55405d3806401a220fd4360c6287c09f72
Victor Paredes <victor@paredescreative.com>
2023-12-18 09:17:32 -0800

    updating

--- sections/standard_template_about_section.php
+++ sections/standard_template_about_section.php
@@ -18,7 +18,7 @@
     <?php the_sub_field('about_title'); ?>
 </h1>
 
-<div class="card-body">
+<div class="card-body" style="background-color: <?php the_sub_field('body_background_color') ?>">
 
     <?php if ( get_sub_field('tagline') ) { ?>
         <h2 style="margin: 0in 0in 10pt; text-align: left; line-height: 112%; font-size: 12pt; font-family: Arial, Helvetica, sans-serif;" type="text/css">
