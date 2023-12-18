775fc05f5161d6c30f98a3b3477b939d7cc48751
Victor Paredes <victor@paredescreative.com>
2023-12-18 15:27:37 -0800

    updating

--- templates/single-landing-page.php
+++ templates/single-landing-page.php
@@ -139,7 +139,7 @@ get_template_part( 'parts/js_header' ); ?>
 <?php if (is_user_logged_in() == 1) { ?>
     <div id="js_tools_wrap">
             <div style="color: #fff; display: inline-block;">
-                Toolbox - 
+                Tools >>  
             </div>
             <div id="copyButton" class="tools_button">
                 Copy Code
