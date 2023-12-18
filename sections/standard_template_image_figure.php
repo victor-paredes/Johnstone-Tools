aa39cfe91cd43e0fc82b338377a59f9cf33c1881
Victor Paredes <victor@paredescreative.com>
2023-12-18 13:53:25 -0800

    updating

--- sections/standard_template_image_figure.php
+++ sections/standard_template_image_figure.php
@@ -1,6 +1,14 @@
 
 <div class="<?php the_sub_field('css_classes') ?>">
-    <img style="width: 100%;" src="<?php the_sub_field('image_file') ?>">
+    <img style="width: 100%;" src="
+                                    <?php 
+                                    if ( get_sub_field('image_remote_url') ) { 
+                                        the_sub_field('image_remote_url'); 
+                                    } else { 
+                                        the_sub_field('image_file'); 
+                                    } ?>
+                                ">
 </div>
 
 
+ 
