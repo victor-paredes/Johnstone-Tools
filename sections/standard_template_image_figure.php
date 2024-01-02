bfc5c7585c1e2f47b65b659d675d2133655006bb
Victor Paredes <victor@paredescreative.com>
2024-01-02 11:17:07 -0800

    updating

--- sections/standard_template_image_figure.php
+++ sections/standard_template_image_figure.php
@@ -1,13 +1,15 @@
 
 <div class="<?php the_sub_field('css_classes') ?>">
-    <img style="width: 100%;" src="
-                                    <?php 
-                                    if ( get_sub_field('image_remote_url') ) { 
-                                        the_sub_field('image_remote_url'); 
-                                    } else { 
-                                        the_sub_field('image_file'); 
-                                    } ?>
-                                ">
+    <a href="<?php if ( get_sub_field('link') ) { the_sub_field('llink'); } ?>">
+        <img style="width: 100%;" src="
+                                        <?php 
+                                        if ( get_sub_field('image_remote_url') ) { 
+                                            the_sub_field('image_remote_url'); 
+                                        } else { 
+                                            the_sub_field('image_file'); 
+                                        } ?>
+                                    ">
+    </a>
 </div>
 
 
