9ee55239fdecc6762ba374c80aafa373256781bd
Victor Paredes <victor@paredescreative.com>
2024-01-25 12:06:51 -0800

    updating

--- sections/standard_template_text_columns.php
+++ sections/standard_template_text_columns.php
@@ -66,7 +66,7 @@
 
             <?php if ( get_sub_field('card_style') == 'pill' ) { ?>
                     .<?php echo $text_columns_card_deck_class; ?> {
-                        dispaly: grid;
+                        display: grid;
                     }
                     .<?php echo $text_columns_card_deck_class; ?> .card {
                         display: flex;
