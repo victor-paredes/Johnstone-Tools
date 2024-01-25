3625dca3cb4ceb7a31eb17d104fd5789a2bc0449
Victor Paredes <victor@paredescreative.com>
2024-01-25 08:19:06 -0800

    updating

--- sections/standard_template_text_columns.php
+++ sections/standard_template_text_columns.php
@@ -33,7 +33,7 @@
               
                             <?php the_sub_field('column_copy'); ?>
                             <br>
-                            <?php the_sub_field('card_style'); ?>
+                            Card Style: <?php the_sub_field('card_style'); ?>
 
 
                 </div> <!-- END .card -->
