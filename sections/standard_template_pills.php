11f1fecd162e11bcf95761fcea9d515cda83ff4b
Victor Paredes <victor@paredescreative.com>
2024-01-25 12:57:01 -0800

    updating

--- sections/standard_template_pills.php
+++ sections/standard_template_pills.php
@@ -1,23 +1,42 @@
+<div class="pills_wrapper">
 
 
 
 
 
-<?php
-    while( have_rows('pills_repeater') ){ 
-        the_row(); 
-?>
 
-        
-    <?php echo get_sub_field('pill_label'); ?>
+    <?php
+        while( have_rows('pills_repeater') ){ 
+            the_row(); 
+    ?>
 
-
-
-<?php
-    } // END while( have_rows('copy') )
-?>
+        <div class="">
+            <?php echo get_sub_field('pill_label'); ?>
+        </div>
 
 
+
+    <?php
+        } // END while( have_rows('copy') )
+    ?>
+
+
+
+
+
+
+
+</div>
 
 
  
+<style>
+    .pills_wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+    }
+    .pills_wrapper div {
+        width: 25%;
+    }
+</style>
