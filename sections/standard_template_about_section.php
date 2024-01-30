48c251551e0e108dcdb95b9eeba87cc21c793c57
Victor Paredes <victor@paredescreative.com>
2024-01-30 13:23:55 -0800

    updating

--- sections/standard_template_about_section.php
+++ sections/standard_template_about_section.php
@@ -14,21 +14,21 @@
 <div class="card" style="">
 <div>
     
-<h1 style="background-color: <?php the_sub_field('title_background_color'); ?>; color: #fff; font-size: 15.0pt; padding: 20px 1.25rem 0 1.25rem; margin-bottom: 0;">
-    <?php the_sub_field('about_title'); ?>
+<h1 style="background-color: <?php echo get_sub_field('title_background_color'); ?>; color: #fff; font-size: 15.0pt; padding: 20px 1.25rem 0 1.25rem; margin-bottom: 0;">
+    <?php echo get_sub_field('about_title'); ?>
 </h1>
 
-<div class="card-body" style="background-color: <?php the_sub_field('body_background_color') ?>; color: <?php the_sub_field('body_text_color') ?>;">
+<div class="card-body" style="background-color: <?php echo get_sub_field('body_background_color') ?>; color: <?php echo get_sub_field('body_text_color') ?>;">
 
     <?php if ( get_sub_field('tagline') ) { ?>
         <h2 style="margin: 0in 0in 10pt; text-align: left; line-height: 112%; font-size: 12pt; font-family: Arial, Helvetica, sans-serif;" type="text/css">
             <strong>
-                <?php the_sub_field('tagline'); ?>
+                <?php echo get_sub_field('tagline'); ?>
             </strong>
         </h2>
     <?php } ?>
 
-    <?php the_sub_field('about_us_description'); ?>
+    <?php echo get_sub_field('about_us_description'); ?>
 
      
 
@@ -46,7 +46,7 @@
 
 
         <div class="embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" src="<?php the_sub_field('video_url'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+          <iframe class="embed-responsive-item" src="<?php echo get_sub_field('video_url'); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
         </div>
 
                 </div>
