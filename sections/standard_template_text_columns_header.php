f6288409dd81271a6cd92988c6043022ad65494e
Victor Paredes <victor@paredescreative.com>
2024-01-30 08:43:19 -0800

    updating

--- sections/standard_template_text_columns_header.php
+++ sections/standard_template_text_columns_header.php
@@ -21,6 +21,11 @@
      
                 <div class="card" style="background-color: transparent; border: initial;">
                     <div class="padding_mobile text_columns_header_text_card pt-5 pl-5">
+                        <?php if ( get_sub_field('notice_type') == 'information' ) { ?>
+                            <span class="material-symbols-outlined">
+                                info
+                            </span>
+                        <?php } ?>
                         <?php the_sub_field('text_content'); ?>
                     </div>
                 </div> <!-- END .card -->
